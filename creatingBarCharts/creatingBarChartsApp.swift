f53e8236b545ac5ad7f5c4cf321fa6985663f78c
Yukun Xie <yxie@lcs.on.ca>
2024-11-04 14:25:11 -0500

    Initial Commit

---
+++ creatingBarCharts/creatingBarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  creatingBarChartsApp.swift
+//  creatingBarCharts
+//
+//  Created by Yukun Xie on 2024/11/4.
+//
+
+import SwiftUI
+
+@main
+struct creatingBarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
